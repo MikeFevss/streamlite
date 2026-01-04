dd26699e7aa3d92dd62475b1e36d4b310752e041
MikeFevss <migaking25@gmail.com>
2026-01-04 07:56:48 +0000

    Update streamlitBingo.py

--- streamlitBingo.py
+++ streamlitBingo.py
@@ -144,19 +144,20 @@ for row_start in range(0, num_boards, boards_per_row):
                 row_html += f'''
                 <div style="
                     display:inline-block;
-                    width:80px; 
+                    width:75px; 
                     height:50px; 
                     border:1px solid #000;
                     background-color:{color};
                     color:black;
                     text-align:center;
                     vertical-align:middle;
-                    padding:2px;
-                    font-size:11px;
+                    padding:1px;
+                    font-size:9px;
                     overflow:hidden;
                 ">{cell}</div>
                 '''
             col.markdown(row_html, unsafe_allow_html=True)
+
 
 
 
